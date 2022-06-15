01977df6dbb9c2ff20ef599574d5aee879b8ea0e
Reuben Simpson <useyourtimewell@gmail.com>
2022-06-16 11:19:23 +1200

    Updated exercise 2

--- week-6/wednesday/src/App.js
+++ week-6/wednesday/src/App.js
@@ -9,10 +9,10 @@ import Exercise2 from "./Exercise2";
 function App() {
   return (
     <div className="App">
-      {/* <Form></Form> */}
+      <Form></Form>
       {/* <CondRendering></CondRendering> */}
       {/* <Exercise1></Exercise1> */}
-      <Exercise2></Exercise2>
+      {/* <Exercise2></Exercise2> */}
     </div>
   );
 }
