bfcceeb1a769b4a415a805fb805c3103946906b6
Reuben Simpson <useyourtimewell@gmail.com>
2022-06-22 17:23:05 +1200

    Added today's MUI exercise

--- week-7/login-modal/src/components/LoginModal.js
+++ week-7/login-modal/src/components/LoginModal.js
@@ -1,45 +1,195 @@
-// NOT COMPLETED YET
-import * as React from "react";
+import { useState, forwardRef } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+// Styling for the Modal Box
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 500, // Changed width from 400 to 500
   bgcolor: "background.paper",
   border: "2px solid #000",
+  borderRadius: "10px", // Added a border radius to make it curved
   boxShadow: 24,
   p: 4,
+  display: "flex", // Added the display:flex property
+  justifyContent: "space-around", // Added the justifyContent property
 };
 
+// Our optional custom theme
+const theme = createTheme({
+  typography: {
+    h6: {
+      color: "#d40808",
+    },
+  },
+});
+
 export default function LoginModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [redShirtsEmail, setRedShirtsEmail] = useState("");
+  const [redShirtsPassword, setRedShirtsPassword] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState({});
+  const [backDropOpen, setBackDropOpen] = useState(false);
+  const [snackOpen, setSnackOpen] = useState(false);
+
+  // ---------------- FOR MUI ALERT ----------------
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  // ---------------- HANDLER FUNCTIONS ----------------
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+  const handleSnackClose = () => setSnackOpen(false);
+
+  // Handling what happens when user types in email text box for RED SHIRTS
+  const handleRedEmailChange = (e) => {
+    setRedShirtsEmail(e.target.value);
+
+    setLoggedInUser({
+      password: redShirtsPassword,
+      email: e.target.value,
+    });
+  };
+  // Handling what happens when user types in password text box for RED SHIRTS
+  const handleRedPasswordChange = (e) => {
+    setRedShirtsPassword(e.target.value);
+    setLoggedInUser({ password: e.target.value, email: redShirtsEmail });
+  };
+
+  // CLEANER SOLUTION: Handling the email and password states all in one event handler function (for BLUE SHIRTS)
+  // We are achieving the same thing here as what we did with red shirts, but cleaner (we don't even need separate field variables)
+  const handleBlueInputs = (e) => {
+    setLoggedInUser((prevState) => {
+      // in our state updating function, we always have access to the previous state (you can call it anything you like)
+      return { ...prevState, [e.target.name]: e.target.value }; // return what is already in the loggedInUser object, and add/replace to it
+    });
+  };
+  // Why using square brackets above for e.target.name again?
+  // let a = "aKey";
+  // let myObj = { [a]: 10 }; // We always use square brackets when we need to compute a dynamic value.
+  // // output will be {aKey:10}
+  // console.log(myObj);
+
+  // For when the user hits either login button
+  const handleLogin = (e) => {
+    if (e.target.name === "redshirts") {
+      // Do something with loggedInUser (ie send credentials to database)
+      console.log(loggedInUser);
+    }
+    if (e.target.name === "blueshirts") {
+      // Do something with loggedInUser (ie send credentials to database)
+      console.log(loggedInUser);
+    }
+    setBackDropOpen(true);
+    setTimeout(() => {
+      setBackDropOpen(false);
+      setSnackOpen(true);
+      setModalOpen(false);
+    }, 3000); // excecute the callback function in setTimeout after 3 seconds
+  };
 
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}
+    >
+      <Button variant="outlined" onClick={handleModalOpen}>
+        Click to login
+      </Button>
+
+      {/* Added ThemeProvider because we've added a custom theme, otherwise we wouldn't require this */}
+      <ThemeProvider theme={theme}>
+        <Modal open={modalOpen} onClose={handleModalClose}>
+          <Box sx={style}>
+            <Stack spacing={2}>
+              {/* Notice that our h6 variant has our custom colour that we applied to it via our theme! */}
+              <Typography variant="h6">Red Shirts Login</Typography>
+              <TextField
+                label="Email address"
+                variant="outlined"
+                size="small"
+                sx={{ m: 1 }}
+                onChange={handleRedEmailChange}
+                name="email"
+              />
+              <TextField
+                label="Password"
+                variant="outlined"
+                size="small"
+                type="password"
+                sx={{ m: 1 }}
+                onChange={handleRedPasswordChange}
+                name="password"
+              />
+              <Button
+                name="redshirts"
+                onClick={handleLogin}
+                variant="contained"
+              >
+                Login
+              </Button>
+            </Stack>
+            <Stack spacing={2}>
+              {/* Notice that our h6 variant's colour has been overridden by the 'sx' prop */}
+              <Typography sx={{ color: "blue" }} variant="h6">
+                Blue Shirts Login
+              </Typography>
+              <TextField
+                label="Email address"
+                variant="outlined"
+                size="small"
+                sx={{ m: 1 }}
+                onChange={handleBlueInputs}
+                name="email" // added a name property so we can access it in our handleBlueInputs function
+              />
+              <TextField
+                label="Password"
+                variant="outlined"
+                size="small"
+                type="password"
+                sx={{ m: 1 }}
+                onChange={handleBlueInputs}
+                name="password" // added a name property so we can access it in our handleBlueInputs function
+              />
+              <Button
+                name="blueshirts"
+                onClick={handleLogin}
+                variant="contained"
+              >
+                Login
+              </Button>
+            </Stack>
+            <Backdrop open={backDropOpen}>
+              <CircularProgress />
+            </Backdrop>
+          </Box>
+        </Modal>
+      </ThemeProvider>
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackClose}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+        <Alert
+          onClose={handleSnackClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Welcome, {loggedInUser.email || loggedInUser.email}!
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
