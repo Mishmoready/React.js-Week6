84d9d6b518e214ffa7fb9d2ecefbe21aca6675c7
Reuben Simpson <useyourtimewell@gmail.com>
2022-08-25 12:57:47 +1200

    Added js code from first js day

---
+++ week-2/js-intro/app.js
@@ -0,0 +1,38 @@
+console.log("55 Hello World from the external javascript file!");
+console.log(1 + 5 + 6);
+
+const firstName = "Reuben";
+console.log(firstName);
+// console.log(firstName); // Single line comment
+console.log(firstName);
+/* Multi-line comment
+console.log(firstName);
+console.log(firstName);
+*/
+console.log(firstName);
+console.log(firstName);
+
+let year = 2022;
+console.log(year);
+
+// year = 2025;
+console.log(year);
+
+let lastName;
+lastName = "Rogers";
+
+let sallysName = "Sally",
+  sallysLastName = "Rogers";
+
+console.log(sallysName, sallysLastName);
+
+const firstAndLastName = "Reuben Simpson";
+let age = 99;
+
+// My first variables!
+console.log(firstAndLastName, age);
+
+const ageCalc = year - 1993;
+console.log(firstAndLastName, age, ageCalc);
+
+console.log((year * age) / year);
