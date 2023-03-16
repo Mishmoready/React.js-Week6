262bdc1b60079f9f039ceca75ca6579d0b589086
Sebin Benjamin <sebinbenjamin@gmail.com>
2023-03-17 10:57:59 +1300

    feat: complete form submission app

---
+++ week-09/day-4-full-stack-app/forms-project/frontend/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('loginForm');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginResultDiv = document.getElementById('loginResult');
+
+form.addEventListener('submit', sendLogin); // When the button is pressed you call sendLogin()
+
+function sendLogin(e) {
+  console.log('Someone clicked submit and sendLogin() is called');
+  // prevent page from refreshing when form is submitted
+  e.preventDefault();
+
+  // Calls a POST API to check the login info
+  fetch('http://localhost:4000/loginWithFetch', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email: emailInput.value,
+      password: passwordInput.value,
+    }),
+  }).then((res) => {
+    if (res.status === 200) {
+      loginResultDiv.innerHTML =
+        '<span style="color: green">Login Successful</span>';
+    } else if (res.status === 401) {
+      loginResultDiv.innerHTML = '<span style="color: red">Login Failed</span>';
+    }
+  });
+}
