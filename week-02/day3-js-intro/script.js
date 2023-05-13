ce87ca663fad448e05e2d05f2566cf8e96b5e74e
Reuben Simpson <useyourtimewell@gmail.com>
2023-05-13 19:51:44 +1200

    added Friday's code

--- week-02/day3-js-intro/script.js
+++ week-02/day3-js-intro/script.js
@@ -1,38 +1,34 @@
 console.log("55 Hello World from the external javascript file!");
-console.log(1 + 5 + 6);
 
-const firstName = "Reuben";
-console.log(firstName);
-// console.log(firstName); // Single line comment
-console.log(firstName);
+const myName = "Reuben";
+console.log(myName);
+
+const firstName = "Burt",
+  lastName = "Munro";
+console.log(firstName, lastName);
+
+let year = 2006;
+console.log(year);
+
+year = 2023; // I've reassigned the 'year' variable
+console.log(year);
+
+console.log(sallysFirstName, sallysLastName);
+
+// ---------- Exercise 4 ----------
+const firstAndLastName = "Reuben Simpson";
+let age = 99;
+
+// Single line comment
+
 /* Multi-line comment
 console.log(firstName);
 console.log(firstName);
 */
-console.log(firstName);
-console.log(firstName);
 
-let year = 2022;
-console.log(year);
-
-// year = 2025;
-console.log(year);
-
-let lastName;
-lastName = "Rogers";
-
-let sallysName = "Sally",
-  sallysLastName = "Rogers";
-
-console.log(sallysName, sallysLastName);
-
-const firstAndLastName = "Reuben Simpson";
-let age = 99;
-
+// ---------- Exercise 5 ----------
 // My first variables!
 console.log(firstAndLastName, age);
 
-const ageCalc = year - 1993;
+const ageCalc = year - 1993; // 'year' here comes from the variable we declared in line 13.
 console.log(firstAndLastName, age, ageCalc);
-
-console.log((year * age) / year);
