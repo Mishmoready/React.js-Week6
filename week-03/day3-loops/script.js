16d9c07fda59ce13caa2e4b0b1104e9220f65915
Ewan Zhang <ewanz@missionreadyhq.com>
2023-05-18 16:56:20 +1200

    add w3d4 while loop

--- week-03/day3-loops/script.js
+++ week-03/day3-loops/script.js
@@ -41,3 +41,89 @@ for (const food of foods) {
   console.log(food);
 }
 
+
+//prompt exercise:
+const ageInput = prompt("Please enter your age:");
+const age = parseInt(ageInput);
+//using isNaN() to check if this variable is type of number, if it's a number, will return true as a boolean
+if (isNaN(age)) {
+  console.log("Invalid input. Please enter a valid age.");
+} else {
+  if (age >= 18) {
+    console.log("Cheers! You are legally allowed to drink beer.");
+  } else {
+    console.log("Sorry, you are not old enough to drink beer.");
+  }
+}
+
+
+
+// WHILE LOOP
+// -------------Exercise 4-------------
+let guess;
+const secretNumber = 3;
+while (guess != secretNumber) {
+  guess = Number(prompt("Please pick a number between 1 and 6 🎲"));
+  guess === secretNumber
+    ? console.log(`You guessed it! ${guess} is the secret number.`)
+    : console.log(`Nice try, but try again`);
+}
+
+// -------------Exercise 5-------------
+const myNewString = "Hell@ the#e";
+for (let i = 0; i < myNewString.length; i++) {
+  if (myNewString[i] === "@" || myNewString[i] === "#") {
+    console.log("error! There is a symbol in there. Found a ", myNewString[i]);
+    break; // A "break" will break out of the loop, abandon remaining loop iterations
+  }
+  console.log(myNewString[i]);
+}
+
+// Iterating over properties of an array
+const cars = ["tesla", "Toyota", "ford"];
+// You can loop through an array using a for of loop
+for (const car of cars) {
+  console.log(car); // tesla, jaguar, ford
+}
+
+
+// Iterating over properties of an object
+// for ... in loop example
+const student = {
+  name: "Rob",
+  age: 25,
+  isAdmin: true,
+};
+for (const key in student) {
+  console.log(key); // name, age, isAdmin
+  console.log(student[key]); // Rob, 25, true
+}
+
+// -------------Exercise 7-------------
+const user = {
+  name: "John",
+  age: 5,
+  isAdmin: true,
+};
+
+for (const key in user) {
+  if (key === "age") {
+    console.log(user[key]);
+    break; // optional - quits after you've got the key called age and its value printed.
+  }
+}
+
+// -------------BONUS Exercise 8-------------
+const myFavNumbers = {
+  firstNum: 2,
+  secondNum: 3,
+  thirdNum: 4,
+};
+
+let sum = 0; // 0 + 2 = 2 ; 2 + 3 = 5; 5 + 4 = 9
+for (const key in myFavNumbers) {
+  sum = sum + myFavNumbers[key];
+  // sum += myFavNumbers[key];
+}
+
+console.log(sum, myFavNumbers);
