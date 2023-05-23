21433883b57a32c94547f433386f5a2b9fc28856
Reuben Simpson <useyourtimewell@gmail.com>
2023-05-23 16:06:50 +1200

    Added the rest of today's DOM code

--- week-04/day1-dom/script.js
+++ week-04/day1-dom/script.js
@@ -1,7 +1,10 @@
 console.log("I'm connected");
-// document.body.innerHTML = "<h1>Good morning world</h1>";
+// -------------- The DOM ---------------
+// document.body.innerHTML = "<h1>Hello World</h1>";
 // document.body.style.background = "blue";
 
+// Colour change example
+let currentColour = 0;
 // function changeColour() {
 //   if (document.body.style.background === "blue") {
 //     document.body.style.background = "white";
@@ -10,37 +13,83 @@ console.log("I'm connected");
 //   }
 // }
 
-let currentColour = 0;
 function changeColour() {
-  const colourArray = ["white", "blue", "red", "green", "yellow", "orange"];
-
+  const colourArray = ["white", "blue", "red", "green", "yellow", "orange", "pink"];
   if (currentColour === colourArray.length - 1) {
     currentColour = 0;
   } else {
     currentColour++;
   }
+  console.log(document.body.style.background);
   document.body.style.background = colourArray[currentColour];
 }
-console.log(document);
 
-// Selectors
-document.querySelector(".header-class").textContent = "Look! My text content has changed.";
-document.querySelector("#header-id").style.fontSize = "75px";
+// Common Selectors (these examples are all selecting the same element)
+const headerEl = document.getElementById("header-id");
+document.querySelector("h1");
+document.querySelector("#header-id");
+document.querySelector(".header-class");
 
-document.getElementById("header-id").style.color = "green";
-document.getElementsByClassName("header-class")[0].style.fontSize = "80px";
-console.log(document.getElementsByClassName("header-class"));
-
-const helloWorldHeader = document.getElementById("helloWorldHeader");
-helloWorldHeader.style.fontSize = "10px";
-helloWorldHeader.textContent = "Hello to you all.";
-
-const counterEl = document.getElementById("counter");
+headerEl.style.fontSize = "30px";
+headerEl.textContent = "Look! My text content has changed.";
 
 // Exercise 1
+const counterEl = document.getElementById("counter");
+
 let counter = 0;
 function incrementByOne() {
   counter++;
-  console.log(counter);
-  counterEl.textContent = `Counter: ${counter}`;
+  counterEl.textContent = counter;
 }
+
+// ----------- Event Listeners -----------
+// myDiv Event listener example
+const myDiv = document.getElementById("divId");
+
+myDiv.addEventListener("mouseover", function () {
+  myDiv.style.background = "blue";
+});
+
+//// What if we wanted to add this practically the event listener to another element?
+//// Well, right now we will violate the Don't Repeat Yourself (DRY) principle
+// someOtherElement.addEventListener("mouseover", function () {
+// someOtherElement.style.background = "blue"; // practically the same as the above event listener
+// });
+
+//// So, we create an external function that we can call in as many event listeners as we want.
+//// But, as it is currently written, it will only ever affect the myDiv element - not very resueable.
+// function changeToPurple() {
+//   myDiv.style.background = "purple";
+// }
+
+// A function that we can use for more than one event listener that is now DYNAMIC using the event.target property
+function changeToPurple(event) {
+  console.log(event);
+  console.log(event.target);
+  event.target.style.background = "purple";
+}
+
+myDiv.addEventListener("mouseout", changeToPurple);
+headerEl.addEventListener("mouseover", changeToPurple); // we defined helloWorldHeader earlier
+// ----------------------------------------
+
+// Exercise 2
+const inputEl = document.querySelector("input");
+const characterCount = document.querySelector("#character-count");
+inputEl.addEventListener("input", logValue);
+function logValue(e) {
+  console.log(e.target.value.length);
+  characterCount.textContent = e.target.value.length;
+}
+
+// Exercise 3
+const userInput1 = document.getElementById("user-input1");
+const userInput2 = document.getElementById("user-input2");
+const sumResult = document.getElementById("sum-result");
+const sumBtn = document.getElementById("sum-btn");
+
+sumBtn.addEventListener("click", function () {
+  sumResult.textContent = `Result: ${userInput1.value} + ${userInput2.value} = ${
+    Number(userInput1.value) + Number(userInput2.value)
+  }`;
+});
